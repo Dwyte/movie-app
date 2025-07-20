0065195c9e08a700be1e5ccebb5d6369b536fd8b
Xander Dwight <xdwightm@proton.me>
2025-07-20 20:20:39 +0800

    Switched to Netflix-like look/themew

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
 import { getTrendingMovies, updateSearchCount } from "./appwrite";
+import { useRef } from "react";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -79,46 +80,62 @@ const App = () => {
   }, []);
 
   return (
-    <main className="bg-gray-100">
-      <header className="flex justify-between fixed left-0 right-0 top-0 items-center bg-white border-b border-b-1 border-gray-300 py-4 px-8">
+    <main className="">
+      <header className="flex justify-between fixed left-0 right-0 top-0 items-center bg-black py-4 px-8">
         <div className="flex justify-between items-center gap-16">
           <img className="w-30 h-9" src="/logo.png" alt="logo" />
-          <a className="text-red-700 font-bold" href="">Movies</a>
-          <a href="">TV Shows</a>
+          <a className="text-white" href="">
+            Movies
+          </a>
+          <a className="text-gray-500">TV Shows</a>
         </div>
 
         <div className="flex justify-center items-center gap-8">
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-          <img className="w-10 h-10" src="/profile-picture.jpg" alt="Smiley Icon" />
+          <img
+            className="w-10 h-10"
+            src="/profile-picture.jpg"
+            alt="Smiley Icon"
+          />
         </div>
       </header>
-      {trendingMovies.length > 0 && (
-        <section className="trending">
-          <h2>Trending Movies</h2>
-          <ul>
-            {trendingMovies.map((movie, index) => (
-              <li key={movie.$id}>
-                <p>{index + 1}</p>
-                <img src={movie.poster_url} alt={movie.title} />
-              </li>
-            ))}
-          </ul>
-        </section>
-      )}
-      <section className="all-movies">
-        <h2 className="">All Movies</h2>
-        {isLoading ? (
-          <Spinner />
-        ) : errorMessage ? (
-          <p className="text-red-500">{errorMessage}</p>
-        ) : (
-          <ul>
-            {movieList.map((movie) => (
-              <MovieCard key={movie.id} movie={movie} />
-            ))}
-          </ul>
+      <div className="py-25 px-10">
+        {trendingMovies.length > 0 && searchTerm.length === 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+            <ul className="flex items-center gap-3">
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id} className="flex items-start">
+                  <p>{index + 1}</p>
+                  <img
+                    className="w-50 h-auto movie-poster"
+                    src={movie.poster_url}
+                    alt={movie.title}
+                  />
+                </li>
+              ))}
+            </ul>
+          </section>
         )}
-      </section>
+        <section className="all-movies">
+          <h2 className="">
+            {searchTerm ? `Showing results for "${searchTerm}"` : "All movies"}
+          </h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul
+              className="grid grid-cols-5 gap-5"
+            >
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
     </main>
   );
 };
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,8 +4,8 @@ function MovieCard({
   movie: { title, vote_average, poster_path, release_date, original_language },
 }) {
   return (
-    <div className="movie-card">
       <img
+      className="movie-poster w-75 h-auto"
         src={
           poster_path
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
@@ -13,24 +13,6 @@ function MovieCard({
         }
         alt={title}
       />
-
-      <div className="mt-4">
-        <h3>{title}</h3>
-        <div className="content">
-          <div className="rating">
-            <img src="star.svg" alt="star icon" />
-            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
-          </div>
-
-          <span>&bull;</span>
-          <p className="lang">{original_language}</p>
-          <span>&bull;</span>
-          <p className="year">
-            {release_date ? release_date.split("-")[0] : "N/A"}
-          </p>
-        </div>
-      </div>
-    </div>
   );
 }
 
