df3922c028e6aa91d64ee0518606d5b45dc23aa7
Xander Dwight <xdwightm@proton.me>
2025-07-22 01:25:02 +0800

    Changed orientation of MovieCard posters to landscape layout

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,16 +1,18 @@
 import { Movie } from "../types";
 
-const MovieCard = ({ movie: { poster_path, title } }: { movie: Movie }) => {
+const MovieCard = ({ movie: { backdrop_path, title } }: { movie: Movie }) => {
+  const movieImageSource = backdrop_path
+    ? `https://image.tmdb.org/t/p/w500/${backdrop_path}`
+    : "/no-movie.png";
+
   return (
-    <img
-      className="movie-poster w-75 h-auto"
-      src={
-        poster_path
-          ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-          : "/no-movie.png"
-      }
-      alt={title}
-    />
+    <div className="movie-poster relative rounded-sm overflow-hidden">
+      <img className="w-75 h-auto" src={movieImageSource} alt={title} />
+
+      <div className="flex items-end bg-[linear-gradient(to_top,_black_0%,_transparent_30%)] font-sans tracking-wide text-white absolute top-0 left-0 right-0 bottom-0 px-1">
+        <div>{title}</div>
+      </div>
+    </div>
   );
 };
 
