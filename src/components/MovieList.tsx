d53cf0c077daf55283ce7c69a256efe2ec48536d
Xander Dwight <xdwightm@proton.me>
2025-07-22 19:11:45 +0800

    Added a simple horizonal scroll control for MovieList

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Movie, MovieGenre } from "../types";
 import MovieCard from "./MovieCard";
 import { getMovies } from "../tmdbAPI";
@@ -9,6 +9,7 @@ interface Props {
 
 const MovieListByGenre = ({ genre }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const scrollContainer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -24,13 +25,37 @@ const MovieListByGenre = ({ genre }: Props) => {
     fetchMovies();
   }, []);
 
+  const handleScroll = (value: number) => {
+    if (scrollContainer === null) return;
+    if (scrollContainer.current === null) return;
+
+    scrollContainer.current.scrollTo({
+      left: scrollContainer.current.scrollLeft + value,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="overflow-hidden">
       <h2>{genre.name}</h2>
-      <div className="flex overflow-hidden gap-3">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+      <div className="relative flex items-center">
+        <div ref={scrollContainer} className="flex overflow-hidden gap-3">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+        <button
+          onClick={() => handleScroll(-1500)}
+          className="absolute left-0 text-white z-5 bg-black cursor-pointer"
+        >
+          Back
+        </button>
+        <button
+          onClick={() => handleScroll(1500)}
+          className="absolute right-0 text-white z-5 bg-black cursor-pointer"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
