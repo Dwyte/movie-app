da231168531acfce31279d45913b842a879194a4
Xander Dwight <xdwightm@proton.me>
2025-07-27 21:48:10 +0800

    add types for TMBD MovieDetails api results

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import { Models } from "appwrite";
+import { countryCodes, languageCodes } from "./constants";
 
 export interface MovieGenre {
   id: number;
@@ -21,6 +22,56 @@ export interface Movie {
   vote_average: number;
   vote_count: number;
 }
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountries {
+  iso_3166_1: CountryCode;
+  name: string;
+}
+
+export interface SpokenLanguages {
+  english_name: string;
+  iso_639_1: LanguageCode;
+  name: string;
+}
+
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: string;
+  budget: number;
+  genres: MovieGenre[];
+  homepage: string;
+  id: number;
+  imbd_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountries[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguages[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: false;
+  vote_average: number;
+  vote_count: number;
+}
+
+export type CountryCode = (typeof countryCodes)[number];
+
+export type LanguageCode = (typeof languageCodes)[number];
 
 export type TrendingMovie = Models.Document & {
   movie_id: number;
