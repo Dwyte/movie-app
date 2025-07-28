90b63ba32057334b6703d28489647dbe9879b7ab
Xander Dwight <xdwightm@proton.me>
2025-07-28 22:06:47 +0800

    add documentation to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation, useMatch } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 import SearchResults from "./pages/SearchResults";
 import MoviePage from "./pages/MoviePage";
@@ -9,22 +9,28 @@ import useIsSmUp from "./hooks/useIsSmUp";
 
 const App = () => {
   const location = useLocation();
-  const state = location.state as { backgroundLocation?: Location };
-  const backgroundLocation = state?.backgroundLocation;
 
+  // Set from the page where the Modal Page was viewed.
+  const backgroundLocation = location.state?.backgroundLocation;
+
+  // Similar behavior to tailwind's sm:
   const isSmUp = useIsSmUp();
-  const isViewingMovie = useMatch("/movie/:id");
 
   return (
     <main>
-      {(!isViewingMovie || isSmUp) && <Header />}
+      <Header />
 
       <div>
+        {/** This renders current location or the origin page
+         * before Modal Page was activated. */}
         <Routes location={isSmUp ? backgroundLocation || location : location}>
-          <Route path="/movie/:movieId" element={<MoviePage />} />
           <Route path="/search" element={<SearchResults />} />
           <Route path="/" element={<Home />} />
+          {/** In mobile, render the MoviePage as standalone page. */}
+          <Route path="/movie/:movieId" element={<MoviePage />} />
         </Routes>
+
+        {/** This renders the MoviePage on top of origin component */}
         {isSmUp && backgroundLocation && (
           <Routes>
             <Route path="/movie/:movieId" element={<MoviePage />} />
