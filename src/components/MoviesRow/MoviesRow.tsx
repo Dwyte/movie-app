e936c15edc837361a3b9f483194a734b4faea351
Xander Dwight <xdwightm@proton.me>
2025-07-29 21:48:56 +0800

    refractor: extract ScrollButtons, improve MovieRow, add docs

---
+++ src/components/MoviesRow/MoviesRow.tsx
@@ -0,0 +1,129 @@
+import { useEffect, useRef, useState } from "react";
+import { Movie } from "../../misc/types";
+import MovieCard from "./../MovieCard";
+import ScrollButton from "./ScrollButton";
+
+const MOVIE_CARD_DIV_WIDTH = 272; // Includes 8px right-gap
+const LEFT_END_SPACE_WIDTH = 48; // Includes 8px right-gap
+const RIGHT_END_SPACE_WIDTH = 16; // 16px, no right-gap
+const TOTAL_SPACE_WIDTH = LEFT_END_SPACE_WIDTH + RIGHT_END_SPACE_WIDTH;
+
+interface Props {
+  title: string;
+  fetchMovies: () => Promise<Movie[]>;
+}
+
+const MoviesRow = ({ title, fetchMovies }: Props) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const scrollableDiv = useRef<HTMLDivElement | null>(null);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const canScrollLeft = currentPage > 0;
+  const canScrollRight = currentPage < totalPages - 1;
+
+  useEffect(() => {
+    const loadTrendingMovies = async () => {
+      try {
+        const response = await fetchMovies();
+        setMovies(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    loadTrendingMovies();
+  }, []);
+
+  const handleResize = () => {
+    const currentDiv = scrollableDiv.current;
+    if (!currentDiv) return;
+
+    // Compute TotalPages and CurrentPage
+    const actualScrollWidth = currentDiv.scrollWidth - TOTAL_SPACE_WIDTH;
+    const visibleMoviesCount = Math.floor(
+      currentDiv.clientWidth / MOVIE_CARD_DIV_WIDTH
+    );
+
+    const step = MOVIE_CARD_DIV_WIDTH * visibleMoviesCount;
+    const newTotalPages = Math.round(actualScrollWidth / step);
+    const newCurrentPage = Math.round(currentDiv.scrollLeft / step);
+
+    setCurrentPage(newCurrentPage);
+    setTotalPages(newTotalPages);
+  };
+
+  useEffect(() => {
+    // Initial compute once the items are loaded.
+    if (movies.length > 0) {
+      handleResize();
+    }
+  }, [movies]);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  /**
+   * Moves the scrollable div to show the targetPage based on direction
+   * @param direction -1 for previous page, 1 next page
+   */
+  const shiftPage = (direction: -1 | 1) => {
+    const currentDiv = scrollableDiv.current;
+    if (currentDiv) {
+      const visibleMoviesCount = Math.floor(
+        currentDiv.clientWidth / MOVIE_CARD_DIV_WIDTH
+      );
+
+      const step = MOVIE_CARD_DIV_WIDTH * visibleMoviesCount;
+      const targetPage = Math.max(
+        0,
+        Math.min(currentPage + direction, totalPages)
+      );
+      setCurrentPage(targetPage);
+      const targetPageScrollLeft = step * targetPage;
+
+      currentDiv.scrollTo({
+        left: targetPageScrollLeft,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    movies.length > 0 && (
+      <section>
+        <h2 className="ml-4 sm:ml-12">{title}</h2>
+        <div className="relative flex items-center group">
+          <ScrollButton
+            direction="left"
+            onClick={() => shiftPage(-1)}
+            isVisible={canScrollLeft}
+          />
+          <ScrollButton
+            direction="right"
+            onClick={() => shiftPage(1)}
+            isVisible={canScrollRight}
+          />
+          <div
+            ref={scrollableDiv}
+            className="flex items-center gap-2 scrollable"
+          >
+            {/** Acts as left padding, also being scrolled so items go through the edges of the screen.*/}
+            <div className="shrink-0 w-2 sm:w-10"></div>
+
+            {movies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+
+            {/** Acts as right padding/space when the user reaches the last page. */}
+            <div className="shrink-0 w-2 sm:w-4"></div>
+          </div>
+        </div>
+      </section>
+    )
+  );
+};
+
+export default MoviesRow;
---
+++ src/components/MoviesRow/ScrollButton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+
+interface Props {
+  direction: "left" | "right";
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  isVisible: boolean;
+}
+
+const icons = {
+  left: BsChevronCompactLeft,
+  right: BsChevronCompactRight,
+};
+
+const styles = {
+  left: "justify-start left-0 bg-linear-to-r from-black to-black/0",
+  right: "justify-end right-0 bg-linear-to-l from-black to-black/0",
+};
+
+const ScrollButton = ({ direction, onClick, isVisible = true }: Props) => {
+  const Icon = icons[direction];
+  return (
+    <div
+      className={`${
+        isVisible ? "hidden group-hover:flex" : "hidden"
+      } absolute items-center text-white w-16 h-36 ${styles[direction]}`}
+    >
+      <button onClick={onClick} className="px-2 h-full cursor-pointer">
+        <Icon className="text-2xl" />
+      </button>
+    </div>
+  );
+};
+
+export default ScrollButton;
