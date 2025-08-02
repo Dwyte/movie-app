022b2e2c339b6579fd26fc52a65ee5a98a47cf2e
Xander Dwight <xdwightm@proton.me>
2025-08-03 00:50:03 +0800

    style: add a dedicated style/layout in MediaPageEpisodesSection for desktop screens

--- src/pages/MediaPage/MediaPageEpisodesSection.tsx
+++ src/pages/MediaPage/MediaPageEpisodesSection.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Season, TVSeasonDetailsAPIResult } from "../../misc/types";
 import { getTVSeasonDetails } from "../../misc/tmdbAPI";
-import { getTMDBImageURL } from "../../misc/utils";
+import {
+  getDurationString,
+  getTMDBImageURL,
+  shortenParagraph,
+} from "../../misc/utils";
 
 interface Props {
   mediaId: number | null;
@@ -37,7 +41,7 @@ const MediaPageEpisodesSection = ({ mediaId, seasons }: Props) => {
   if (!seasons) return;
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-2 sm:gap-4">
       <div className="flex items-center">
         <h2 className="flex-1 m-0">{seasons[selectedSeason].name}</h2>
         <select
@@ -47,25 +51,46 @@ const MediaPageEpisodesSection = ({ mediaId, seasons }: Props) => {
         >
           {seasons.map((season, index) => (
             <option key={season.id} value={index}>
-              Episode {season.season_number}
+              Season {season.season_number}
             </option>
           ))}
         </select>
       </div>
 
-      <div className="flex flex-col gap-2 max-h-100 sm:max-h-200 scrollable">
+      <div className="flex flex-col max-h-100 sm:max-h-200 scrollable">
         {seasonDetails?.episodes.map((episode) => (
-          <div key={episode.id} className="flex gap-4">
+          <div
+            key={episode.id}
+            className="flex items-center gap-4 py-2 sm:border-b-1 sm:border-stone-700 sm:py-4 sm:pr-6 cursor-pointer hover:bg-stone-900"
+          >
+            <span className="hidden sm:block text-right w-8 text-lg text-stone-200">
+              {episode.episode_number}
+            </span>
             <img
-              src={getTMDBImageURL(episode.still_path, "200")}
-              className="rounded-sm"
+              src={
+                episode.still_path
+                  ? getTMDBImageURL(episode.still_path, "200")
+                  : "/no-image-landscape.png"
+              }
+              className="aspect-16/10 object-cover rounded-sm max-w-[200px]"
               alt=""
             />
-            <div className="flex flex-col justify-center">
-              <div className="text-sm uppercase font-bold text-red-800">
+            <div className="flex flex-col flex-1 gap-1 justify-center">
+              <div className="text-sm uppercase font-bold text-red-800 sm:hidden">
                 Episode {episode.episode_number}
               </div>
-              <div>{episode.name}</div>
+              <div className="flex">
+                <p className="sm:font-bold sm:flex-1 sm:text-lg">
+                  {episode.name}
+                </p>
+
+                <p className="hidden sm:block sm:font-bold sm:text-stone-400">
+                  {getDurationString(episode.runtime)}
+                </p>
+              </div>
+              <p className="hidden sm:block text-sm text-stone-400">
+                {shortenParagraph(episode.overview, 100)}
+              </p>
             </div>
           </div>
         ))}
