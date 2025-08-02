a8f55a9a7b8500c0c368d9dfce73ae4a088da3eb
Xander Dwight <xdwightm@proton.me>
2025-08-02 20:59:26 +0800

    chore: move MoviePageDetailsSection to its own module

--- src/pages/MediaPage/MediaPage.tsx
+++ src/pages/MediaPage/MediaPage.tsx
@@ -1,30 +1,19 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
-import { RiDownloadLine } from "react-icons/ri";
-import {
-  BsBadgeCcFill,
-  BsBadgeHdFill,
-  BsPlayFill,
-  BsPlusLg,
-  BsSend,
-  BsStar,
-} from "react-icons/bs";
+import { BsPlusLg, BsSend, BsStar } from "react-icons/bs";
 
 import {
-  MediaImage,
   MediaDetails,
   MediaType,
   MediaCreditsAPIResult,
-  Crew,
 } from "../../misc/types";
-
-import { getDurationString, shortenParagraph } from "../../misc/utils";
 
 import { getMediaItemCredits, getMediaItemDetails } from "../../misc/tmdbAPI";
 
 import useIsSmUp from "../../hooks/useIsSmUp";
 import RelatedMediaSection from "./RelatedMediaSection";
 import MediaPageHeroSection from "./MediaPageHeroSection";
+import MediaPageDetailsSection from "./MediaPageDetailsSection";
 
 interface Props {
   mediaType: MediaType;
@@ -34,7 +23,7 @@ const MediaPage = ({ mediaType }: Props) => {
   const [mediaItemDetails, setMediaItemDetails] = useState<MediaDetails | null>(
     null
   );
-  const [mediaCredits, setMediaCredits] =
+  const [mediaItemCredits, setMediaItemCredits] =
     useState<MediaCreditsAPIResult | null>(null);
 
   const modalRef = useRef<HTMLDivElement | null>(null);
@@ -63,7 +52,7 @@ const MediaPage = ({ mediaType }: Props) => {
       setMediaItemDetails(_mediaItemDetails);
 
       const _mediaCasts = await getMediaItemCredits(mediaType, mediaId);
-      setMediaCredits(_mediaCasts);
+      setMediaItemCredits(_mediaCasts);
     };
 
     fetchMediaDetails();
@@ -72,18 +61,6 @@ const MediaPage = ({ mediaType }: Props) => {
   const closeModal = () => {
     navigate(backgroundLocation);
   };
-
-  const director = useMemo<Crew | null>(() => {
-    if (!mediaCredits) return null;
-
-    const directorDetails = mediaCredits.crew.filter(
-      (crew) => crew.job === "Director"
-    );
-
-    if (directorDetails.length > 0) return directorDetails[0];
-
-    return null;
-  }, [mediaCredits]);
 
   useEffect(() => {
     const resetScroll = () => {
@@ -117,77 +94,11 @@ const MediaPage = ({ mediaType }: Props) => {
               {mediaItemDetails && mediaItemDetails.title}
             </h1>
           )}
-          <div className="flex flex-col gap-2 sm:flex sm:flex-row sm:gap-12">
-            <div className="flex flex-col gap-2 sm:flex-3">
-              <div className="flex items-center gap-2 text-stone-400">
-                <div>
-                  {mediaItemDetails?.release_date ||
-                    mediaItemDetails?.first_air_date}
-                </div>
-                <div>
-                  {mediaItemDetails?.runtime &&
-                    getDurationString(mediaItemDetails.runtime)}
 
-                  {mediaItemDetails?.number_of_seasons &&
-                    `${mediaItemDetails.number_of_seasons} season${
-                      mediaItemDetails.number_of_seasons > 1 ? "s" : ""
-                    }`}
-                </div>
-                <BsBadgeHdFill className="text-xl" />
-                <BsBadgeCcFill className="text-xl" />
-              </div>
-
-              <div className="flex items-center gap-2">
-                <div className="flex flex-col items-center leading-none text-[8px] font-bold bg-red-600 p-[3px] rounded-[2px]">
-                  <span>TOP</span> <span className="text-[11px]">10</span>
-                </div>
-                <h3 className="font-bold text-lg">#1 in TV Shows Today</h3>
-              </div>
-
-              <button className="primary-btn justify-center sm:hidden">
-                <BsPlayFill className="text-2xl mr-1" />
-                Play
-              </button>
-              <button className="secondary-btn justify-center sm:hidden">
-                <RiDownloadLine className="text-2xl mr-1" />
-                Download
-              </button>
-
-              <p className="text-stone-300 text-sm">
-                {mediaItemDetails &&
-                  shortenParagraph(mediaItemDetails.overview, 200)}
-              </p>
-            </div>
-
-            <div className="flex flex-col gap-2 sm:flex-2">
-              {mediaCredits && mediaCredits?.cast.length > 0 && (
-                <div className="text-sm text-stone-400">
-                  Casts: &#32;
-                  <span className="text-white">
-                    {mediaCredits.cast
-                      .slice(0, 3)
-                      .map((cast) => cast.name)
-                      .join(", ")}
-                  </span>
-                </div>
-              )}
-              {director && (
-                <div className="text-sm text-stone-400">
-                  Director: &#32;
-                  <span className="text-white">{director.name}</span>
-                </div>
-              )}
-              <div className="text-sm text-stone-400">
-                Genres: &#32;
-                <span className="text-white">
-                  {mediaItemDetails &&
-                    mediaItemDetails.genres
-                      .map((genre) => genre.name)
-                      .join(", ")}
-                </span>
-              </div>
-            </div>
-          </div>
+          <MediaPageDetailsSection
+            mediaItemDetails={mediaItemDetails}
+            mediaItemCredits={mediaItemCredits}
+          />
 
           <div className="flex gap-2 sm:hidden">
             <button className="flex flex-col justify-between items-center gap-1 px-3 py-2 min-w-16">
---
+++ src/pages/MediaPage/MediaPageDetailsSection.tsx
@@ -0,0 +1,101 @@
+import { useMemo } from "react";
+import { RiDownloadLine } from "react-icons/ri";
+
+import { BsBadgeCcFill, BsBadgeHdFill, BsPlayFill } from "react-icons/bs";
+
+import { Crew, MediaCreditsAPIResult, MediaDetails } from "../../misc/types";
+import { getDurationString, shortenParagraph } from "../../misc/utils";
+
+interface Props {
+  mediaItemDetails: MediaDetails | null;
+  mediaItemCredits: MediaCreditsAPIResult | null;
+}
+
+const MediaPageDetailsSection = ({
+  mediaItemDetails,
+  mediaItemCredits,
+}: Props) => {
+  const director = useMemo<Crew | null>(() => {
+    if (!mediaItemCredits) return null;
+
+    const directorDetails = mediaItemCredits.crew.filter(
+      (crew) => crew.job === "Director"
+    );
+
+    if (directorDetails.length > 0) return directorDetails[0];
+
+    return null;
+  }, [mediaItemCredits]);
+
+  return (
+    <div className="flex flex-col gap-2 sm:flex sm:flex-row sm:gap-12">
+      <div className="flex flex-col gap-2 sm:flex-3">
+        <div className="flex items-center gap-2 text-stone-400">
+          <div>
+            {mediaItemDetails?.release_date || mediaItemDetails?.first_air_date}
+          </div>
+          <div>
+            {mediaItemDetails?.runtime &&
+              getDurationString(mediaItemDetails.runtime)}
+
+            {mediaItemDetails?.number_of_seasons &&
+              `${mediaItemDetails.number_of_seasons} season${
+                mediaItemDetails.number_of_seasons > 1 ? "s" : ""
+              }`}
+          </div>
+          <BsBadgeHdFill className="text-xl" />
+          <BsBadgeCcFill className="text-xl" />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <div className="flex flex-col items-center leading-none text-[8px] font-bold bg-red-600 p-[3px] rounded-[2px]">
+            <span>TOP</span> <span className="text-[11px]">10</span>
+          </div>
+          <h3 className="font-bold text-lg">#1 in TV Shows Today</h3>
+        </div>
+
+        <button className="primary-btn justify-center sm:hidden">
+          <BsPlayFill className="text-2xl mr-1" />
+          Play
+        </button>
+        <button className="secondary-btn justify-center sm:hidden">
+          <RiDownloadLine className="text-2xl mr-1" />
+          Download
+        </button>
+
+        <p className="text-stone-300 text-sm">
+          {mediaItemDetails && shortenParagraph(mediaItemDetails.overview, 200)}
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-2 sm:flex-2">
+        {mediaItemCredits && mediaItemCredits?.cast.length > 0 && (
+          <div className="text-sm text-stone-400">
+            Casts: &#32;
+            <span className="text-white">
+              {mediaItemCredits.cast
+                .slice(0, 3)
+                .map((cast) => cast.name)
+                .join(", ")}
+            </span>
+          </div>
+        )}
+        {director && (
+          <div className="text-sm text-stone-400">
+            Director: &#32;
+            <span className="text-white">{director.name}</span>
+          </div>
+        )}
+        <div className="text-sm text-stone-400">
+          Genres: &#32;
+          <span className="text-white">
+            {mediaItemDetails &&
+              mediaItemDetails.genres.map((genre) => genre.name).join(", ")}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MediaPageDetailsSection;
