b854c0d87524e15ee7b606db1a7e2d864197b93e
Xander Dwight <xdwightm@proton.me>
2025-08-02 22:50:36 +0800

    feat: add MoviePageCasts grid section

--- src/pages/MediaPage/MediaPageCastsSection.tsx
+++ src/pages/MediaPage/MediaPageCastsSection.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { MediaCreditsAPIResult } from "../../misc/types";
+import { getTMDBImageURL } from "../../misc/utils";
 
-const MediaPageCastsSection = () => {
-  return (
-    <div>MediaPageCastsSection</div>
-  )
+interface Props {
+  mediaItemCredits: MediaCreditsAPIResult | null;
 }
 
-export default MediaPageCastsSection
+const MediaPageCastsSection = ({ mediaItemCredits }: Props) => {
+  if (!mediaItemCredits) return;
+
+  return (
+    <div className="grid gap-4 grid-cols-2 sm:grid-cols-4">
+      {mediaItemCredits.cast.slice(0, 10).map((cast) => {
+        return (
+          <div className="flex flex-col gap-2" key={cast.id}>
+            <img
+              className="rounded-sm aspect-square object-cover"
+              src={
+                cast.profile_path
+                  ? getTMDBImageURL(cast.profile_path, "200")
+                  : "/profile-picture.jpg"
+              }
+              alt={cast.name}
+            />
+            <div>
+              <p>{cast.name}</p>
+              <p className="text-stone-400">
+                {cast.character.replaceAll("'", "")}
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MediaPageCastsSection;
