555525c1cb1a9a2782e7c9852e0807f627678db1
Xander Dwight <xdwightm@proton.me>
2025-08-03 20:34:44 +0800

    feat: add tvshows in search results

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,58 +1,46 @@
-import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 import MediaCard from "../components/MediaCard";
 
 import { getSearchMediaItems } from "../misc/tmdbAPI";
+import { useQuery } from "@tanstack/react-query";
 import { Media } from "../misc/types";
 
 const SearchResults = () => {
-  const [errorMessage, setErrorMessage] = useState<string>("");
-  const [results, setResults] = useState<Media[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get("q");
 
-  const searchMediaItems = async (query: string) => {
-    setIsLoading(true);
-    setErrorMessage("");
+  const {
+    data: results,
+    error,
+    isLoading,
+  } = useQuery<Media[]>({
+    enabled: !!searchQuery,
+    queryKey: ["search", searchQuery],
+    queryFn: async ({ queryKey }) => {
+      const [_, searchQuery] = queryKey as [string, string];
+      const movieResults = (await getSearchMediaItems("movie", searchQuery))
+        .results;
+      const tvResults = (await getSearchMediaItems("tv", searchQuery)).results;
+      return [...movieResults, ...tvResults];
+    },
+  });
 
-    try {
-      const data = await getSearchMediaItems("movie", query);
-      setResults(data.results || []);
-    } catch (error) {
-      console.log(`Error searching media ${error}`);
-      if (error instanceof Error) {
-        setErrorMessage(error.message);
-      } else {
-        setErrorMessage("Unknown Error.");
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const query = searchParams.get("q");
-
-  useEffect(() => {
-    if (query) {
-      searchMediaItems(query);
-    }
-  }, [searchParams]);
+  if (isLoading) return <div className="mt-100 text-white">Searching...</div>;
+  if (!results) return;
 
   return (
-    query && (
-      <div className="py-20 sm:py-24 px-4 sm:px-16">
-        <h1 className="hidden sm:block text-2xl text-stone-500 sm:mb-8">
-          Search Results for: <span className="text-white">"{query}"</span>
-        </h1>
+    <div className="py-20 sm:py-24 px-4 sm:px-16">
+      <h1 className="hidden sm:block text-2xl text-stone-500 sm:mb-8">
+        Search Results for: <span className="text-white">"{searchQuery}"</span>
+      </h1>
 
-        <div className="grid grid-cols-3 gap-2 sm:grid-cols-6 sm:gap-y-8">
-          {results.map((media) => (
-            <MediaCard media={media} flexible />
-          ))}
-        </div>
+      <div className="grid grid-cols-3 gap-2 sm:grid-cols-6 sm:gap-y-8">
+        {results.map((media) => (
+          <MediaCard key={media.id} media={media} flexible />
+        ))}
       </div>
-    )
+    </div>
   );
 };
 
