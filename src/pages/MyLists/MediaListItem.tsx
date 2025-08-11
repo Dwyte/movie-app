a538b16c2efde82aeb8aa1518bbf7f84113f5542
Xander Dwight <xdwightm@proton.me>
2025-08-11 12:33:30 +0800

    feat: add handleOnClick in MediaItemList for navigating to MediaPage

--- src/pages/MyLists/MediaListItem.tsx
+++ src/pages/MyLists/MediaListItem.tsx
@@ -3,8 +3,9 @@ import { Media } from "../../misc/types";
 import FiveStarRating from "../../components/FiveStarRating";
 
 import { MEDIA_TYPE_NAME, NO_IMAGE_LANDSCAPE_PATH } from "../../misc/constants";
-import { getGenreNameFromId, getTMDBImageURL } from "../../misc/utils";
+import { getTMDBImageURL } from "../../misc/utils";
 import { BsXLg } from "react-icons/bs";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface Props {
   media: Media;
@@ -15,8 +16,20 @@ const MediaListItem = ({ media }: Props) => {
     ? getTMDBImageURL(media.backdrop_path)
     : NO_IMAGE_LANDSCAPE_PATH;
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleOnClick = () => {
+    navigate(`/${media.media_type}/${media.id}`, {
+      state: { backgroundLocation: location },
+    });
+  };
+
   return (
-    <div className="flex items-center justify-between w-full">
+    <div
+      onClick={handleOnClick}
+      className="flex items-center justify-between w-full"
+    >
       <div className="flex w-100 items-center gap-2">
         <img src={thumbnail} alt="" className="w-36" />
         <div className="flex flex-col sm:flex-col">
