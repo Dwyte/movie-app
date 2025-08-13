4fa9f0f93c11b9cd5bd8f179fd2d2d696c445fc9
Xander Dwight <xdwightm@proton.me>
2025-08-13 18:30:55 +0800

    style: add comments and rating on mobile layout for ListPage MediaListItem

--- src/pages/MyLists/MediaListItem.tsx
+++ src/pages/MyLists/MediaListItem.tsx
@@ -2,10 +2,20 @@ import { Media, MediaRef } from "../../misc/types";
 
 import FiveStarRating from "../../components/FiveStarRating";
 
-import { MEDIA_TYPE_NAME, NO_IMAGE_LANDSCAPE_PATH } from "../../misc/constants";
+import {
+  MEDIA_TYPE_NAME,
+  NO_IMAGE_LANDSCAPE_PATH,
+  NO_IMAGE_PORTRAIT_PATH,
+} from "../../misc/constants";
 import { getTMDBImageURL } from "../../misc/utils";
-import { BsChatLeftDots, BsXLg } from "react-icons/bs";
+import {
+  BsChatLeftDots,
+  BsChatSquareDots,
+  BsChatText,
+  BsXLg,
+} from "react-icons/bs";
 import { useLocation, useNavigate } from "react-router-dom";
+import useIsSmUp from "../../hooks/useIsSmUp";
 
 interface Props {
   media: Media;
@@ -22,9 +32,14 @@ const MediaListItem = ({
   onDelete,
   onComment,
 }: Props) => {
-  const thumbnail = media.backdrop_path
-    ? getTMDBImageURL(media.backdrop_path)
-    : NO_IMAGE_LANDSCAPE_PATH;
+  const isSmUp = useIsSmUp();
+
+  const thumbnailPath = isSmUp ? media.backdrop_path : media.poster_path;
+  const noImagePath = isSmUp ? NO_IMAGE_LANDSCAPE_PATH : NO_IMAGE_PORTRAIT_PATH;
+
+  const thumbnail = thumbnailPath
+    ? getTMDBImageURL(thumbnailPath)
+    : noImagePath;
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -38,50 +53,70 @@ const MediaListItem = ({
   const mediaRef = { media_id: media.id, media_type: media.media_type };
 
   return (
-    <div onClick={handleOnClick} className="flex items-center w-full gap-16">
-      <div className="flex w-100 items-center gap-2">
-        <img src={thumbnail} alt="" className="w-36" />
-        <div className="flex flex-col sm:flex-col">
-          <div>{media.title}</div>
-          <div className="text-stone-500 text-sm">
-            {MEDIA_TYPE_NAME[media.media_type]}
+    <div onClick={handleOnClick} className="flex items-center w-full gap-2">
+      <img
+        src={thumbnail}
+        alt=""
+        className="h-32 sm:h-24 rounded-sm sm:rounded-none"
+      />
+
+      <div className="flex flex-col gap-4 flex-1">
+        <div className="flex gap-8 items-center justify-between">
+          <div className="flex flex-col sm:w-50 sm:flex-col">
+            <div className="text-sm sm:text-base">{media.title}</div>
+            <div className="text-stone-500 text-xs sm:text-sm">
+              {MEDIA_TYPE_NAME[media.media_type]}
+            </div>
+          </div>
+
+          <div className="hidden sm:flex justify-between flex-1">
+            <div className="hidden lg:flex gap-4 items-center">
+              <div className="text-white">
+                {new Date(media.first_air_date || media.release_date || 0)
+                  .getFullYear()
+                  .toString()}
+              </div>
+              <FiveStarRating rating={media.vote_average} />
+            </div>
+            {comment && (
+              <div className="text-right italic tracking-wide cursor-text">
+                "{comment}"
+              </div>
+            )}
+          </div>
+          <div
+            className="flex gap-2 sm:gap-4 ms-center justify-end"
+            onClick={(e) => e.stopPropagation()}
+          >
+            {onComment && (
+              <button
+                onClick={() => onComment(media)}
+                className="secondary-icon-btn p-2"
+                disabled={isDeleting}
+              >
+                <BsChatSquareDots />
+              </button>
+            )}
+            {onDelete && (
+              <button
+                onClick={() => onDelete(mediaRef)}
+                className="secondary-icon-btn p-2"
+                disabled={isDeleting}
+              >
+                <BsXLg />
+              </button>
+            )}
           </div>
         </div>
-      </div>
 
-      <div className="hidden flex-1 sm:flex gap-4 items-center">
-        <div className="text-white">
-          {new Date(media.first_air_date || media.release_date || 0)
-            .getFullYear()
-            .toString()}
+        <div className="flex flex-col gap-2 sm:hidden text-sm">
+          <FiveStarRating rating={media.vote_average} />
+          {comment && (
+            <div className="flex-1 italic tracking-wide cursor-text">
+              "{comment}"
+            </div>
+          )}
         </div>
-        <FiveStarRating rating={media.vote_average} />
-        {comment && (
-          <div className="flex-1 text-right italic tracking-wide cursor-text">"{comment}"</div>
-        )}
-      </div>
-      <div
-        className="flex gap-4 items-center justify-end"
-        onClick={(e) => e.stopPropagation()}
-      >
-        {onComment && (
-          <button
-            onClick={() => onComment(media)}
-            className="cursor-pointer flex items-center justify-center hover:text-red-600 text-lg"
-            disabled={isDeleting}
-          >
-            <BsChatLeftDots />
-          </button>
-        )}
-        {onDelete && (
-          <button
-            onClick={() => onDelete(mediaRef)}
-            className="cursor-pointer flex items-center justify-center hover:text-red-600 text-lg"
-            disabled={isDeleting}
-          >
-            <BsXLg />
-          </button>
-        )}
       </div>
     </div>
   );
