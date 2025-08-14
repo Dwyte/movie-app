c3651d282363b4d9558d9b190984f10603b00900
Xander Dwight <xdwightm@proton.me>
2025-08-14 21:10:19 +0800

    feat: improve skeleton loaders for MediaPageCastsSection to wait for all images to load before showing them all at once.

--- src/pages/MediaPage/MediaPageCastsSection.tsx
+++ src/pages/MediaPage/MediaPageCastsSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MediaCreditsAPIResult } from "../../misc/types";
 import { getTMDBImageURL } from "../../misc/utils";
 import Skeleton from "../../components/Skeleton";
@@ -7,37 +7,60 @@ interface Props {
   mediaItemCredits: MediaCreditsAPIResult | null;
 }
 
+const CastSkeleton = ({ className }: React.ComponentProps<"div">) => {
+  return (
+    <div className={`flex flex-col gap-2 ${className}`}>
+      <Skeleton className="aspect-square" />
+      <Skeleton className="h-6 w-[80%]" />
+      <Skeleton className="h-6 w-[50%]" />
+    </div>
+  );
+};
+
 const MediaPageCastsSection = ({ mediaItemCredits }: Props) => {
+  const [doneLoadingCount, setDoneLoadingCount] = useState(0);
+
+  const mediaItemCasts = mediaItemCredits?.cast.slice(0, 10);
+
+  const isLoadingImages =
+    !mediaItemCasts || doneLoadingCount !== mediaItemCasts.length;
+
   return (
     <div>
       <div className="grid gap-4 grid-cols-2 sm:grid-cols-4">
+        {/** Optimistic Skeletons even before mediaItemCredits are fetched  */}
         {!mediaItemCredits &&
-          Array.from({ length: 9 }, (_, k) => (
-            <div key={k} className="flex flex-col gap-2">
-              <Skeleton className="aspect-[1/1]" />
-              <Skeleton className="h-8 w-[80%]" />
-              <Skeleton className="h-6 w-[50%]" />
-            </div>
-          ))}
+          Array.from({ length: 9 }, (_, k) => <CastSkeleton key={k} />)}
 
-        {mediaItemCredits &&
-          mediaItemCredits.cast.slice(0, 10).map((cast) => {
+        {mediaItemCasts &&
+          mediaItemCasts.map((cast) => {
             return (
-              <div className="flex flex-col gap-2" key={cast.id}>
-                <img
-                  className="rounded-sm aspect-square object-cover"
-                  src={
-                    cast.profile_path
-                      ? getTMDBImageURL(cast.profile_path, "200")
-                      : "/profile-picture.jpg"
-                  }
-                  alt={cast.name}
-                />
-                <div>
-                  <p>{cast.name}</p>
-                  <p className="text-stone-400">
-                    {cast.character.replaceAll("'", "")}
-                  </p>
+              <div key={cast.id} className="relative">
+                {/** Skeletons while we wait for all Images to load, then show all at the same time. */}
+                {isLoadingImages && (
+                  <CastSkeleton className="absolute inset-0" />
+                )}
+                <div
+                  className={`flex flex-col gap-2 transition-opacity ${
+                    isLoadingImages ? "opacity-0" : "opacity-100"
+                  }`}
+                >
+                  <img
+                    className={`rounded-sm aspect-square object-cover`}
+                    src={
+                      cast.profile_path
+                        ? getTMDBImageURL(cast.profile_path, "200")
+                        : "/profile-picture.jpg"
+                    }
+                    onLoad={() => setDoneLoadingCount((p) => p + 1)}
+                    alt={cast.name}
+                  />
+                  <div>
+                    <p>{cast.name}</p>
+                    <p className="text-stone-400">
+                      {cast.character.replaceAll("'", "")}
+                    </p>
+                  </div>
                 </div>
               </div>
             );
