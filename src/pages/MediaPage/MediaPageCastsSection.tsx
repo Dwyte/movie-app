1e14603de97ff67845f3227b507bb8444035137c
Xander Dwight <xdwightm@proton.me>
2025-08-14 20:29:12 +0800

    style: add empty list/data placeholder for Casts and Crew

--- src/pages/MediaPage/MediaPageCastsSection.tsx
+++ src/pages/MediaPage/MediaPageCastsSection.tsx
@@ -9,38 +9,45 @@ interface Props {
 
 const MediaPageCastsSection = ({ mediaItemCredits }: Props) => {
   return (
-    <div className="grid gap-4 grid-cols-2 sm:grid-cols-4">
-      {!mediaItemCredits &&
-        Array.from({ length: 9 }, (_, k) => (
-          <div key={k} className="flex flex-col gap-2">
-            <Skeleton className="aspect-[1/1]" />
-            <Skeleton className="h-8 w-[80%]" />
-            <Skeleton className="h-6 w-[50%]" />
-          </div>
-        ))}
-
-      {mediaItemCredits &&
-        mediaItemCredits.cast.slice(0, 10).map((cast) => {
-          return (
-            <div className="flex flex-col gap-2" key={cast.id}>
-              <img
-                className="rounded-sm aspect-square object-cover"
-                src={
-                  cast.profile_path
-                    ? getTMDBImageURL(cast.profile_path, "200")
-                    : "/profile-picture.jpg"
-                }
-                alt={cast.name}
-              />
-              <div>
-                <p>{cast.name}</p>
-                <p className="text-stone-400">
-                  {cast.character.replaceAll("'", "")}
-                </p>
-              </div>
+    <div>
+      <div className="grid gap-4 grid-cols-2 sm:grid-cols-4">
+        {!mediaItemCredits &&
+          Array.from({ length: 9 }, (_, k) => (
+            <div key={k} className="flex flex-col gap-2">
+              <Skeleton className="aspect-[1/1]" />
+              <Skeleton className="h-8 w-[80%]" />
+              <Skeleton className="h-6 w-[50%]" />
             </div>
-          );
-        })}
+          ))}
+
+        {mediaItemCredits &&
+          mediaItemCredits.cast.slice(0, 10).map((cast) => {
+            return (
+              <div className="flex flex-col gap-2" key={cast.id}>
+                <img
+                  className="rounded-sm aspect-square object-cover"
+                  src={
+                    cast.profile_path
+                      ? getTMDBImageURL(cast.profile_path, "200")
+                      : "/profile-picture.jpg"
+                  }
+                  alt={cast.name}
+                />
+                <div>
+                  <p>{cast.name}</p>
+                  <p className="text-stone-400">
+                    {cast.character.replaceAll("'", "")}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+      {mediaItemCredits && mediaItemCredits.cast.length === 0 && (
+        <div className="text-lg text-stone-400 font-bold p-8 w-full text-center">
+          No Data Available.
+        </div>
+      )}
     </div>
   );
 };
