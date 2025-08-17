d1c36be47bd35383f5e20320fe0dce9c584d0a26
Xander Dwight <xdwightm@proton.me>
2025-08-17 20:17:06 +0800

    style: new accent color style for toast.

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -6,9 +6,9 @@ const positionStyles = {
 };
 
 const accentColorStyles = {
-  success: "from-green-900 via-stone-900",
-  warning: "from-amber-900 via-stone-900",
-  error: "from-red-900 via-stone-900",
+  success: "from-green-900 via-green-900/50 to-stone-900",
+  warning: "from-amber-900 via-amber-900/50 to-stone-900",
+  error: "from-red-900 via-red-900/50 to-stone-900",
 };
 
 export type ToastAccentColor = keyof typeof accentColorStyles;
@@ -24,7 +24,7 @@ interface Props {
 const Toast = ({ children, position, accentColor }: Props) => {
   return (
     <div
-      className={`fade-in fixed bg-linear-to-t via-40% to-100% to-stone-900 ${accentColorStyles[accentColor]} ${positionStyles[position]} w-max shadow-xl shadow-black/75 font-bold rounded-sm overflow-hidden transform translate-[-50%] p-6 text-white z-9999999`}
+      className={`fade-in fixed bg-linear-to-r via-10% to-30% bg-stone-900 ${accentColorStyles[accentColor]} ${positionStyles[position]} w-max shadow-xl shadow-black/75 font-bold rounded-sm overflow-hidden transform translate-[-50%] p-6 text-white z-9999999`}
     >
       <div>{children}</div>
     </div>
