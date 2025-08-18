3e809b49dcd7d682a0e385412dec1dbd95cc1f7f
Xander Dwight <xdwightm@proton.me>
2025-08-18 18:27:51 +0800

    style: fix border bug in ListContainer. A 1px border black appears after changing main-bg from black.

--- src/pages/MyLists/ListContainer.tsx
+++ src/pages/MyLists/ListContainer.tsx
@@ -7,7 +7,7 @@ const ListContainer = ({
   return (
     <div
       {...props}
-      className={className || "shrink-0 flex flex-col sm:border-t-stone-800 sm:border-1"}
+      className={className || "shrink-0 flex flex-col sm:border-t-stone-800 sm:border-t-1"}
     />
   );
 };
