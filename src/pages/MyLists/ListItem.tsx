85bf0ac92fe37160e8805aba386f03a3ebcc6645
Xander Dwight <martindwyte@gmail.com>
2025-08-31 01:19:27 +0800

    style: fix list items showing bullet markers when it should not

--- src/pages/MyLists/ListItem.tsx
+++ src/pages/MyLists/ListItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const ListItem = ({ children, index }: Props) => {
   return (
-    <div className="group sm:flex sm:items-center sm:gap-4 text-white py-1 cursor-pointer sm:border-b-[var(--list-border-color)] sm:border-b-1 sm:py-[2px] list-item">
+    <div className="group sm:flex sm:items-center sm:gap-4 text-white py-1 cursor-pointer sm:border-b-[var(--list-border-color)] sm:border-b-1 sm:py-[2px] hover:bg-linear-[to_bottom,#1f1f1f_0%,#202020_10%,#202020_90%,#1f1f1f]">
       <ListItemHoverIndicator />
       {index && (
         <div className="hidden sm:flex items-center justify-center text-stone-500 font-bold w-12">
