657a2b2b76fcb9a3844e06b30aba4c0e9be07c31
Xander Dwight <martindwyte@gmail.com>
2025-09-02 22:09:31 +0800

    chore: move Link wrapper to MediaCardThumbnail instead of using it as the main parent container for MediaCard.
    - avoid having buttons inside an anchor tag

--- src/components/MediaCard/MediaCard.tsx
+++ src/components/MediaCard/MediaCard.tsx
@@ -60,29 +60,35 @@ const MediaCard = ({
     });
   };
 
+  const handleMouseEnter = () => {
+    setIsCardActive(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsCardActive(false);
+  };
+
+  const handleFocus = () => {
+    setIsCardActive(true);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLDivElement>) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) setIsCardActive(false);
+  };
+
   const [isCardActive, setIsCardActive] = useState(false);
 
   return (
-    <Link
-      aria-label={mediaLabel}
-      // Goto MediaPage and set backgroundLocation to tell what page to render
-      // at the background when rendering the Modal MediaPage in desktop.
-      to={mediaUrl}
-      state={{
-        backgroundLocation,
-        showTrailerOnLoad: false,
-      }}
-      className={`group/mcard hover:z-50 focus:z-50 focus-within:z-50 relative flex items-center justify-center shrink-0 cursor-pointer ${
+    <div
+      className={`group/mcard [:is(:hover,:focus,:focus-within)]:z-50 relative flex items-center justify-center shrink-0 ${
         flexible
           ? "w-full h-full aspect-[1/1.5] sm:aspect-[16/9]"
           : MEDIA_CARD_DIMENSIONS
       }`}
-      onMouseEnter={() => setIsCardActive(true)}
-      onMouseLeave={() => setIsCardActive(false)}
-      onFocus={() => setIsCardActive(true)}
-      onBlur={(e) => {
-        if (!e.currentTarget.contains(e.relatedTarget)) setIsCardActive(false);
-      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
     >
       {/** If flexible we have a static div container that will take the full space available.
        * This is for grids where dimensions depends on defined cols and not exactly from width
@@ -91,13 +97,25 @@ const MediaCard = ({
        * static div and scale up on hover etc. */}
 
       <div
-        className={`absolute group rounded-sm overflow-hidden transition-transform group-hover/mcard:scale-115  group-hover/mcard:drop-shadow-md/100 group-hover/mcard:drop-shadow-black group-focus/mcard:scale-115 group-focus/mcard:drop-shadow-md/100 group-focus/mcard:drop-shadow-black group-focus-within/mcard:scale-115 group-focus-within:/mcard:drop-shadow-md/100 group-focus-within:/mcard:drop-shadow-black`}
+        className={`absolute group rounded-sm overflow-hidden transition-transform 
+          ${isCardActive && "scale-115 drop-shadow-md drop-shadow-black"}`}
       >
-        <MediaCardThumbnail
-          media={media}
-          onLoad={onImageLoad}
-          flexible={flexible}
-        />
+        <Link
+          // Goto MediaPage and set backgroundLocation to tell what page to render
+          // at the background when rendering the Modal MediaPage in desktop.
+          to={mediaUrl}
+          state={{
+            backgroundLocation,
+            showTrailerOnLoad: false,
+          }}
+          aria-label={mediaLabel}
+        >
+          <MediaCardThumbnail
+            media={media}
+            onLoad={onImageLoad}
+            flexible={flexible}
+          />
+        </Link>
 
         {isCardActive && (
           <MediaCardDetails
@@ -107,7 +125,7 @@ const MediaCard = ({
           />
         )}
       </div>
-    </Link>
+    </div>
   );
 };
 
