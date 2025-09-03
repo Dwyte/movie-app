9604064b271ed85960d69c0bc7475a889abcff0c
Xander Dwight <martindwyte@gmail.com>
2025-09-03 22:01:20 +0800

    chore: add focus-within bg style on ListItemDiv

--- src/components/Lists/ListItemDiv/ListItemDiv.tsx
+++ src/components/Lists/ListItemDiv/ListItemDiv.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const ListItemDiv = ({ children, index }: Props) => {
   return (
-    <div className="text-white py-1 cursor-pointer sm:flex sm:items-center sm:gap-4 sm:border-b-[var(--list-border-color)] sm:border-b-1 sm:py-[2px] group-hover:bg-(image:--list-item-hover-bg) group-focus:bg-(image:--list-item-hover-bg)">
+    <div className="text-white py-1 cursor-pointer sm:flex sm:items-center sm:gap-4 sm:border-b-[var(--list-border-color)] sm:border-b-1 sm:py-[2px] group-hover:bg-(image:--list-item-hover-bg) group-focus:bg-(image:--list-item-hover-bg) group-focus-within:bg-(image:--list-item-hover-bg)">
       <ListItemHoverIndicator />
       {index && (
         <div className="hidden sm:flex items-center justify-center text-stone-500 font-bold w-12">
