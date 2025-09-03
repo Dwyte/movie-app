72e3a87b83996aff388a7b18fe07b00878943916
Xander Dwight <martindwyte@gmail.com>
2025-09-04 02:38:09 +0800

    chore: fix a11y issues on EditListBackdrop
    - make backdrop picker usable on keyboard

--- src/pages/ListPage/EditListBackdrop.tsx
+++ src/pages/ListPage/EditListBackdrop.tsx
@@ -19,8 +19,8 @@ const EditListBackdrop = ({
   return (
     <div className="flex flex-col gap-4">
       <h2 className="m-0">Choose Cover Photo:</h2>
-      <div
-        className={`grid grid-cols-1 max-w-200 gap-2 max-h-120 scrollable rounded-sm ${
+      <fieldset
+        className={`grid grid-cols-1 max-w-200 gap-2 max-h-120 scrollable p-1 rounded-sm focus-within:outline-2 outline-blue-200 ${
           listResults.length === 2 && "grid-cols-2"
         } ${listResults.length > 3 && "grid-cols-2 sm:grid-cols-3"}
         `}
@@ -35,25 +35,31 @@ const EditListBackdrop = ({
           };
 
           return (
-            <div
+            <label
               key={media.id}
-              onClick={handleClick}
-              className="relative flex items-center justify-center rounded-sm overflow-hidden cursor-pointer hover:brightness-75 transition duration-200"
+              htmlFor={media.id.toString()}
+              className="relative flex items-center justify-center cursor-pointer hover:brightness-75 transition duration-200"
             >
+              <input
+                id={media.id.toString()}
+                type="radio"
+                name="listBackdrop"
+                className="sr-only peer"
+                checked={selectedBackdrop === media.backdrop_path}
+                onChange={handleClick}
+              />
               <img
                 className="rounded-sm"
                 src={getTMDBImageURL(media.backdrop_path)}
               />
 
-              {selectedBackdrop === media.backdrop_path && (
-                <div className="absolute bg-black font-bold text-white w-full p-1 text-center sm:p-2 sm:text-lg">
-                  SELECTED
-                </div>
-              )}
-            </div>
+              <div className="hidden peer-checked:block absolute bg-black font-bold text-white w-full p-1 text-center sm:p-2 sm:text-lg">
+                SELECTED
+              </div>
+            </label>
           );
         })}
-      </div>
+      </fieldset>
     </div>
   );
 };
