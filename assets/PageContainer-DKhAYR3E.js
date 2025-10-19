6fe870d1e23f59d587eb18079ba4befd8ec17159
Dwyte <Dwyte@users.noreply.github.com>
2025-10-19 13:55:59 +0000

    deploy: 7a8d3529888ce80beca018da3cf73dd09a0eea8e

---
+++ assets/PageContainer-DKhAYR3E.js
@@ -0,0 +1,1 @@
+import{j as s,_ as e}from"./index-aO6opLOu.js";const o=r=>s.jsx("div",{...r,className:e("min-h-screen mx-4 sm:mx-20 border-x-1 border-[var(--list-border-color)]",r.className)});export{o as P};
